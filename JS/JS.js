085f30712a835b497fcae8c836e3c7f1a10f5207
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-10-02 10:17:07 -0400

    Update JS.js

--- JS/JS.js
+++ JS/JS.js
@@ -1,20 +1,3 @@
-// Initialization
-let redirect = false;
-
-// Redirect
-function redirectFunc() {
-  if (redirect == true) {
-    if (location.href !== 'https://project-showcase-v3.codeclash.repl.co/Redirect.html') {
-      location.href = 'https://project-showcase-v3.codeclash.repl.co/Redirect.html';
-    }
-  } else {
-    if (location.href === 'https://project-showcase-v3.codeclash.repl.co/Redirect.html') {
-      location.href = 'https://project-showcase-v3.codeclash.repl.co';
-    }
-  }
-}
-redirectFunc();
-window.addEventListener('hashchange', redirectFunc);
 // Version
 function verFunction() {
   var number = "v0.0.8g";
