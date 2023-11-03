3f5853b4dc0f1429f2f6f3c79bf4cbe290af6fee
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-11-02 20:51:51 -0400

    Update JS.js

--- JS/JS.js
+++ JS/JS.js
@@ -1,9 +1,9 @@
 // Version
 function verFunction() {
-  var number = "v0.0.8g";
+  var number = "v0.0.9";
   document.querySelector("#myVer").innerHTML = number;
 }
 function verFunctionNext() {
-  var number = "v0.0.9";
+  var number = "v0.0.9a";
   document.querySelector("#myVerN").innerHTML = number;
 }
