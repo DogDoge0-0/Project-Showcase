0cdd3f7e0ac268bf8df731ae4600ab7c0453771c
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-12-15 09:22:21 -0500

    Update HTML.js

--- JS/HTML.js
+++ JS/HTML.js
@@ -35,89 +35,89 @@ const yourContentArray = [
 	// Unit 1
 	{
 		title: '1.1: Make a Recipe',
-		link: '../HTML/Unit1/1.1.html',
+		link: 'HTML/Unit1/1.1.html',
 		content: 'My first project, featuring the five most basic tags.'
 	},
 	{
 		title: '1.2: Make a Card',
-		link: '../HTML/Unit1/1.2.html',
+		link: 'HTML/Unit1/1.2.html',
 		content: 'My second project, featuring basic JavaScript code.'
 	},
 	{
 		title: '1.3: Tell a Story',
-		link: '../HTML/Unit1/1.4.html',
+		link: 'HTML/Unit1/1.4.html',
 		content: 'This project features my first experience with div tags.'
 	},
 	{
 		title: '1.4: Design a Poster',
-		link: '../HTML/Unit1/1.4.html',
+		link: 'HTML/Unit1/1.4.html',
 		content: 'This project shows off my first use of borders via Cascading Style Sheets.'
 	},
 	{
 		title: '1.5: Make a Robot',
-		link: '../HTML/Unit1/1.5.html',
+		link: 'HTML/Unit1/1.5.html',
 		content: 'This project was my first experience with positioning objects using the absolute positioning style.'
 	},
 	{
 		title: '1.6: Web Showcase',
-		link: '../HTML/Unit1/1.6.html',
+		link: 'HTML/Unit1/1.6.html',
 		content: 'This project is my first iteration of this website, featuring a basic layout.'
 	},
 	{
 		title: '1.7: Team Task - Web Design',
-		link: '../HTML/Unit1/1.7.html',
+		link: 'HTML/Unit1/1.7.html',
 		content: 'This project was my first time doing a project with multiple people, showing off some wacky vehicles.'
 	},
 	// Unit 2
 	{
 		title: '2.1: Build a Town',
-		link: '../HTML/Unit2/2.1.html',
+		link: 'HTML/Unit2/2.1.html',
 		content: 'This was my first time using Scaleable Vector Graphics icons.'
 	},
 	{
 		title: '2.2: Animate a Ferris Wheel',
-		link: '../HTML/Unit2/2.2.html',
+		link: 'HTML/Unit2/2.2.html',
 		content: 'This was the first time I used animations in Cascading Style Sheets.'
 	},
 	{
 		title: '2.3: Guessing Game',
-		link: '../HTML/Unit2/2.3.html',
+		link: 'HTML/Unit2/2.3.html',
 		content: 'This was my first overly ambitious project, which had to be massively scaled down due to lack of time.'
 	},
 	{
 		title: '2.4: Responsive Portfolio',
-		link: '../HTML/Unit2/2.4.html',
+		link: 'HTML/Unit2/2.4.html',
 		content: 'This project was my first time using the Bootstrap framework, which I have continued to use to this day.'
 	},
 	{
 		title: '2.5: Responsive Personal Website',
-		link: '../HTML/Unit2/2.5.html',
+		link: 'HTML/Unit2/2.5.html',
 		content: 'This project is <em>this</em> project.'
 	},
 	// Unit 3
 	{
 		title: '3.1: Intro to JS Tutorial',
-		link: '../HTML/Unit3/3.1.html',
+		link: 'HTML/Unit3/3.1.html',
 		content: 'This was my first time making actual code, in the form of JavaScript.'
 	},
 	{
 		title: '3.2: First JS Website',
-		link: '../HTML/Unit3/3.2.html',
+		link: 'HTML/Unit3/3.2.html',
 		content: 'This was the second time I used JavaScript in an actual website.'
 	},
 	{
 		title: '3.3: Advanced CSS Scene',
-		link: '../HTML/Unit3/3.3.html',
+		link: 'HTML/Unit3/3.3.html',
 		content: 'This was my first proper &#39;game&#39;, about guessing numbers (with a twist).'
 	},
 	{
 		title: '3.4: Responsive Portfolio',
-		link: '../HTML/Unit3/3.4.html',
+		link: 'HTML/Unit3/3.4.html',
 		content: 'This project my second &#39;game&#39;, about guessing colors.'
 	},
 	{
 		title: '3.5: Minigame',
-		link: '../HTML/Unit3/3.5.html',
+		link: 'HTML/Unit3/3.5.html',
 		content: 'This project was a collaborative effort to make an actual game, and we did it. <em>Kind of</em>.'
 	},
 ];
