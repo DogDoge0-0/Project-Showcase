60f8408710c84ab06112e9af69f269e4a50e9ade
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2024-01-08 08:48:17 -0500

    Update JS.js

--- JS/JS.js
+++ JS/JS.js
@@ -3,3 +3,6 @@ function verFunction() {
   var number = "v0.0.9b";
   document.querySelector("#myVer").innerHTML = number;
 }
+document.addEventListener("DOMContentLoaded", function() {
+    verFunction();
+});
